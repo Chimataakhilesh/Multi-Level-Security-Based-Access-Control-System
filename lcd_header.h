01df7d997ecc51a0d7ea773ff9324235d1c01aaf
Chimata Akhilesh <156538616+Chimataakhilesh@users.noreply.github.com>
2026-04-22 22:10:09 +0530

    Update lcd_header.h

--- lcd_header.h
+++ lcd_header.h
@@ -9,49 +9,60 @@ void LCD_COMMAND(u8);
 void LCD_DATA(u8);
 void LCD_STRING(u8 *);
 
-void LCD_INIT(void){
-	IODIR0 = LCD_D|RS|E;
-	LCD_COMMAND(0x01);
-	LCD_COMMAND(0x02);
-	LCD_COMMAND(0x0C);
-	LCD_COMMAND(0x28);
+/* Initialize LCD */
+void LCD_INIT(void)
+{
+    IODIR0 = LCD_D | RS | E;
+    LCD_COMMAND(0x01);  // Clear display
+    LCD_COMMAND(0x02);  // Return home
+    LCD_COMMAND(0x0C);  // Display ON
+    LCD_COMMAND(0x28);  // 4-bit mode
 }
 
-void LCD_COMMAND(u8 cmd){
-	IOCLR0 = LCD_D;
-	IOSET0 = (cmd & 0xf0)<<10;
-	IOCLR0 = RS;
-	IOSET0 = E;
-	delay_ms(2);
-	IOCLR0 = E;
-	
-	IOCLR0 = LCD_D;
-	IOSET0 = (cmd & 0x0f)<<14;
-	IOCLR0 = RS;
-	IOSET0 = E;
-	delay_ms(2);
-	IOCLR0 = E;
+/* Send command to LCD */
+void LCD_COMMAND(u8 cmd)
+{
+    IOCLR0 = LCD_D;
+
+    IOSET0 = (cmd & 0xF0) << 10;
+    IOCLR0 = RS;
+    IOSET0 = E;
+    delay_ms(2);
+    IOCLR0 = E;
+
+    IOCLR0 = LCD_D;
+    IOSET0 = (cmd & 0x0F) << 14;
+    IOCLR0 = RS;
+    IOSET0 = E;
+    delay_ms(2);
+    IOCLR0 = E;
 }
-void LCD_DATA(u8 d){
-	IOCLR0 = LCD_D;
-	IOSET0 = (d & 0xf0)<<10;
-	IOSET0 = RS;
-	IOSET0 = E;
-	delay_ms(2);
-	IOCLR0 = E;
-	
-	IOCLR0 = LCD_D;
-	IOSET0 = (d & 0x0f)<<14;
-	IOSET0 = RS;
-	IOSET0 = E;
-	delay_ms(2);
-	IOCLR0 = E;
+
+/* Send data to LCD */
+void LCD_DATA(u8 d)
+{
+    IOCLR0 = LCD_D;
+
+    IOSET0 = (d & 0xF0) << 10;
+    IOSET0 = RS;
+    IOSET0 = E;
+    delay_ms(2);
+    IOCLR0 = E;
+
+    IOCLR0 = LCD_D;
+    IOSET0 = (d & 0x0F) << 14;
+    IOSET0 = RS;
+    IOSET0 = E;
+    delay_ms(2);
+    IOCLR0 = E;
 }
+
+/* Display string on LCD */
 void LCD_STRING(u8 *s)
 {
-	while(*s)
-	{
-		LCD_DATA(*s++);
-	}
+    while(*s)
+    {
+        LCD_DATA(*s++);
+    }
 }
 

9b3b2bfb20375f32c88c34d11e75c5ec5f19d039
Chimata Akhilesh <156538616+Chimataakhilesh@users.noreply.github.com>
2026-04-22 22:05:31 +0530

    Refactor EEPROM function implementations
    Refactored EEPROM functions for clarity and consistency.

--- EEPROM_header.h
+++ EEPROM_header.h
@@ -1,34 +1,45 @@
-#include<LPC21XX.H>
+#include <LPC21XX.H>
 
-void eeprom_page_write(u8 SA,u8 WDA,u8 *p,u8 nbytes);
-void eeprom_seq_read(u8 SA,u8 RDA,u8 *p,u8 nbytes);
+/* EEPROM function prototypes */
+void eeprom_page_write(u8 SA, u8 WDA, u8 *p, u8 nbytes);
+void eeprom_seq_read(u8 SA, u8 RDA, u8 *p, u8 nbytes);
 
-void eeprom_page_write(u8 SA,u8 WDA,u8 *p,u8 nbytes)
+/* Write data to EEPROM */
+void eeprom_page_write(u8 SA, u8 WDA, u8 *p, u8 nbytes)
 {
-	u8 i;
-	I2C_START();
-	I2C_WRITE(SA<<1);
-	I2C_WRITE(WDA);
-	for(i=0;i<nbytes;i++)
-	{
-		I2C_WRITE(p[i]);
-	}
-	I2C_STOP();
-	delay_ms(10);
+    u8 i;
+
+    I2C_START();
+    I2C_WRITE(SA << 1);   // Slave address + write
+    I2C_WRITE(WDA);       // Memory address
+
+    for(i = 0; i < nbytes; i++)
+    {
+        I2C_WRITE(p[i]);  // Write data
+    }
+
+    I2C_STOP();
+    delay_ms(10);         // Write cycle delay
 }
 
-void eeprom_seq_read(u8 SA,u8 RDA,u8 *p,u8 nbytes)
+/* Read data from EEPROM */
+void eeprom_seq_read(u8 SA, u8 RDA, u8 *p, u8 nbytes)
 {
-	u8 i;
-	I2C_START();
-	I2C_WRITE(SA<<1);
-	I2C_WRITE(RDA);
-	I2C_RESTART();
-	I2C_WRITE((SA<<1)|1);
-	for(i=0;i<nbytes-1;i++)
-	{
-		p[i] = I2C_MASTERACK();
-	}
-	p[i] = I2C_NACK();
-	I2C_STOP();
+    u8 i;
+
+    I2C_START();
+    I2C_WRITE(SA << 1);       // Slave address + write
+    I2C_WRITE(RDA);           // Memory address
+
+    I2C_RESTART();
+    I2C_WRITE((SA << 1) | 1); // Slave address + read
+
+    for(i = 0; i < nbytes - 1; i++)
+    {
+        p[i] = I2C_MASTERACK();   // Read with ACK
+    }
+
+    p[i] = I2C_NACK();            // Last byte
+
+    I2C_STOP();
 }
